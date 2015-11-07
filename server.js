fe9e7f681421a18f0f388622d93340dcc3d4ff6b
Jillian Fetter <jillian.fetter@gmail.com>
2015-11-07 00:02:48 -0800

    set up user profile constructor

--- server.js
+++ server.js
@@ -3,7 +3,17 @@
  var request = require("request");
  var _ = require("lodash"); 
 
+//recieve data from the user interface and create a newProfile
 
+function userProfile (driver, rider, name, email, phone, phone, licPlate, mileagePoints){
+	this.driver = driver// mark true or false depending on current use of ap
+	this.rider = rider// true or false
+	this.name = name
+	this.email = email
+	this.phone = phone
+	this.licPlate = licPlate
+	this.mileagePoints = mileagePoints
+}
 
 
 

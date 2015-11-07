3690d2926f14d5ea03c378e9da02036aa7af59aa
Jillian Fetter <jillian.fetter@gmail.com>
2015-11-07 00:54:42 -0800

    added arrays for keeping hold of driver and rider locations and started a function to identify who is who and where each is

--- server.js
+++ server.js
@@ -4,6 +4,8 @@
  var _ = require("lodash"); 
 
 var users = [];
+var driverLocations = [];
+var riderLocations = [];
 
 //recieve data from the user interface and create a newProfile
 function userProfile (name, email, phone, phone, licPlate, mileagePoints){
@@ -13,6 +15,7 @@ function userProfile (name, email, phone, phone, licPlate, mileagePoints){
 	this.licPlate = licPlate
 	this.mileagePoints = mileagePoints
 }
+
 
 //keep track of the users points
 function managePoints (status, miles) {
@@ -25,6 +28,17 @@ function managePoints (status, miles) {
 
 function addUser (userProfile) {
 	users.push(userProfile);
+}
+
+function updateLocation (status, location) {
+	//push an object that includes userProfile.this.name and location 
+	// to array as either an object or an array within an array 
+	if (status = "driver"){
+			//display color = red
+	}
+	if (status = "rider"){
+			//display color = green
+	}
 }
 
 server.listen(3000);

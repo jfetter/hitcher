398be54ced36060c927b2dba94dfafbe283dc2a6
Alicia Poindexter <alcampbel@gmail.com>
2015-11-07 16:04:59 -0800

    Save my changes

--- main.js
+++ main.js
@@ -2,4 +2,43 @@
 
 // this ref is a reference to the fire base that I just created
 var ref = new Firebase("https://hitcher.firebaseio.com/");
+var request = require("request"); 
+
+var users = [];
+var driverLocations = [];
+var riderLocations = [];
+
+//recieve data from the user interface and create a newProfile
+function userProfile (name, email, phone, phone, licPlate, mileagePoints){
+  this.name = name
+  this.email = email
+  this.phone = phone
+  this.licPlate = licPlate
+  this.mileagePoints = mileagePoints
+}
+
+
+//keep track of the users points
+function managePoints (status, miles) {
+  if (status === "driver"){
+    this.userProfile.mileagePoints += miles;
+  } else{
+    this.userProfile.mileagePoints -= miles;
+  }
+}
+
+function addUser (userProfile) {
+  users.push(userProfile);
+}
+
+function updateLocation (status, location) {
+  //push an object that includes userProfile.this.name and location 
+  // to array as either an object or an array within an array 
+  if (status = "driver"){
+      //display color = red
+  }
+  if (status = "rider"){
+      //display color = green
+  }
+}
 
--- server.js
+++ server.js
@@ -1,85 +1,15 @@
 "use strict";
 
- var request = require("request");
- var _ = require("lodash"); 
+var http = require('http');
+var port = 3000;
 
+var server = http.createServer(function(req, res){
+  res.setHeader('Access-Control-Allow-Origin', '*');
 
+});
+  
+server.listen(port);
+console.log(`Node server listening on port ${port}`);
 
-var users = [{}];
-//front end of site will push to the driver locations and rider locations
-var driverLocations = [];
-var riderLocations = [];
-
-//if username not found in database recieve data from the user interface and create a newProfile
-var userProfile =  URL.userProfile  ? JSON.parse(URL.userProfile) : addUser();
-
-//object constructor for user profiles. 
-function userProfile (name, email, phone, phone, licPlate, mileagePoints){
-	this.name = name
-	this.email = email
-	this.phone = phone
-	this.licPlate = licPlate
-	this.mileagePoints = mileagePoints
-}
-
-function addUser(userProfile) {
-	users.push(userProfile);
-}
-
-function locale (name, locale){
-	this.name = name
-	this.locale = locale
-	this.status = status
-}
- 
-function addUser(userProfile) {
-	users.push(userProfile);
-}
-
-// pass in users.name 
-// find the user in the array of users
-// pass back the entire profile for that user to be passed to
-// the driver that they are paired with
-function findUserInfo (name, array) {
-var ind;
-array.forEach(function(item, index, all){
-var uname = all[index].name
-console.log(uname)
-        if (uname === name) {
-       console.log("match", all.indexOf(item))
-      ind = index;
-      }
-       console.log("miss" + uname)
-  	  
-})
- 		return array[ind]
-} 
-//to find the rider's or driver's profile to give it to the other user
-// call findUserInfo(userName, users)
-// to find location info call findUserInfo(userName, driverLocation) 
-
-
-//keep track of the users points
-function manageMileagePoints (status, miles) {
-	if (status === "driver"){
-		this.userProfile.mileagePoints += miles;
-	} else{
-		this.userProfile.mileagePoints -= miles;
-	}
-}
-
-
-function updateLocation (status, location) {
-	//push an object that includes userProfile.this.name and location 
-	// to array as either an object or an array within an array 
-	if (status = "driver"){
-			//display color = red
-	}
-	if (status = "rider"){
-			//display color = green
-	}
-}
-
-server.listen(3005);
 
 
